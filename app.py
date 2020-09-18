3c9cee43034c8135166ed3d6c25a0351bc0a2515
amira <amira.a.a.hashim@gmail.com>
2020-09-19 01:04:30 +0200

    search feature

--- app.py
+++ app.py
@@ -133,14 +133,18 @@ def search_venues():
   # TODO: implement search on artists with partial string search. Ensure it is case-insensitive.
   # seach for Hop should return "The Musical Hop".
   # search for "Music" should return "The Musical Hop" and "Park Square Live Music & Coffee"
+  search_for = Venue.name.ilike('%{}%'.format(request.form.get('search_term')))
+  search_output = Venue.query.filter(search_for).all()
   response={
-    "count": 1,
-    "data": [{
-      "id": 2,
-      "name": "The Dueling Pianos Bar",
-      "num_upcoming_shows": 0,
-    }]
+    'count': len(search_output),
+    'data':[]
   }
+  for output in search_output:
+    response['data'].append({
+      'id':output.id,
+      'name':output.name,
+      'num_upcoming_shows':output.upcoming_shows_count
+    })
   return render_template('pages/search_venues.html', results=response, search_term=request.form.get('search_term', ''))
 
 @app.route('/venues/<int:venue_id>')
@@ -240,14 +244,18 @@ def search_artists():
   # TODO: implement search on artists with partial string search. Ensure it is case-insensitive.
   # seach for "A" should return "Guns N Petals", "Matt Quevado", and "The Wild Sax Band".
   # search for "band" should return "The Wild Sax Band".
+  search_for = Artist.name.ilike('%{}%'.format(request.form.get('search_term')))
+  search_output = Artist.query.filter(search_for).all()
   response={
-    "count": 1,
-    "data": [{
-      "id": 4,
-      "name": "Guns N Petals",
-      "num_upcoming_shows": 0,
-    }]
+    'count': len(search_output),
+    'data':[]
   }
+  for output in search_output:
+    response['data'].append({
+      'id':output.id,
+      'name':output.name,
+      'num_upcoming_shows':output.upcoming_shows_count
+    })
   return render_template('pages/search_artists.html', results=response, search_term=request.form.get('search_term', ''))
 
 @app.route('/artists/<int:artist_id>')
